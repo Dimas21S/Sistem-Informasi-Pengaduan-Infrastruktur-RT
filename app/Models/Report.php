4ed3a87ebf023f59db1dbfee576e4da67622dc49
Dimas Satyo Wijayanto <dewiandrianiii75@gmail.com>
2025-09-13 17:25:37 +0700

    Add: Model Report, Beranda view, database

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     //
+    protected $fillable = [
+        'judul laporan',
+        'isi laporan',
+        'foto bukti',
+        'status',
+        'tanggal laporan'
+    ];
 }
