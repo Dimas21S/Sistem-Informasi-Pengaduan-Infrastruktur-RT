ff1f00b92ef80b7b2f2a38b3f54ffc246bec2fa0
Dimas Satyo Wijayanto <dewiandrianiii75@gmail.com>
2025-09-16 23:09:04 +0700

    Edit: beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -116,6 +116,14 @@
             </div>
           </div>
           
+          <div class="mt-5">
+            <div class="card-border border border-primary ">
+              <img src= "..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button>Go somewhere</button>
+            </div>
         </div>
       </div>
     </div>
