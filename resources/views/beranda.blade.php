faa05481d5a067e5fbfdf25a0380320233ea0a18
Dimas Satyo Wijayanto <dewiandrianiii75@gmail.com>
2025-09-19 19:17:58 +0700

    Edit: beranda.blade.php

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard Laporan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <style>
       body {
         overflow-x: hidden;
@@ -25,6 +26,10 @@
       }
       .card:hover {
         transform: translateY(-5px);
+      }
+      .card-border {
+        border: 1px solid #0d6efd;
+        border-radius: 0.375rem;
       }
       @media (max-width: 768px) {
         #sidebar {
@@ -62,33 +67,30 @@
         <ul class="nav nav-pills flex-column mb-auto">
           <li class="nav-item">
             <a href="#" class="nav-link active" aria-current="page">
-              <svg class="bi pe-none me-2" width="16" height="16" fill="currentColor">
-                <use xlink:href="#home"/>
-              </svg>
+              <i class="bi bi-house-door-fill"></i>
               Beranda
             </a>
           </li>
           <li>
             <a href="#" class="nav-link link-dark">
-              <svg class="bi pe-none me-2" width="16" height="16" fill="currentColor">
-                <use xlink:href="#speedometer2"/>
-              </svg>
+              <i class="bi bi-card-list"></i>
               Daftar Laporan
             </a>
           </li>
           <li>
             <a href="#" class="nav-link link-dark">
-              <svg class="bi pe-none me-2" width="16" height="16" fill="currentColor">
-                <use xlink:href="#table"/>
-              </svg>
+              <i class="bi bi-person-fill"></i>
               Profil
             </a>
           </li>
         </ul>
         <hr>
-        <li>
-            Log Out</a>
-        </li>
+        <div class="dropdown">
+          <a href="#" class="d-flex align-items-center link-dark text-decoration-none">
+            <i class="bi bi-box-arrow-left me-2"></i>
+            Log Out
+          </a>
+        </div>
       </div>
 
       <!-- Konten Utama -->
@@ -97,50 +99,38 @@
         
         <div class="card-container d-flex flex-wrap justify-content-center">
           <!-- Card laporan sukses -->
-          <div class="card" style="width: 18rem; height: 8rem; border-left-color: green; border-left-width: 5px; border-left-style: solid;">
+          <div class="card" style="width: 18rem; height: 8rem; border-left: 5px solid green;">
             <div class="card-body">
               <h5 class="card-title">Status: Sukses</h5>
               <p class="card-text">42</p>
             </div>
           </div>
-          <div class="card" style="width: 18rem; height: 8rem; border-left-color: orange; border-left-width: 5px; border-left-style: solid;">
+          <div class="card" style="width: 18rem; height: 8rem; border-left: 5px solid orange;">
             <div class="card-body">
               <h5 class="card-title">Status: Proses</h5>
               <p class="card-text">42</p>
             </div>
           </div>
-          <div class="card" style="width: 18rem; height: 8rem; border-left-color: red; border-left-width: 5px; border-left-style: solid;">
+          <div class="card" style="width: 18rem; height: 8rem; border-left: 5px solid red;">
             <div class="card-body">
               <h5 class="card-title">Status: Pending</h5>
               <p class="card-text">42</p>
             </div>
           </div>
+        </div>
           
-          <div class="mt-5">
-            <div class="card-border border border-primary ">
-              <img src= "..." class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button>Go somewhere</button>
+        <div class="mt-5 d-flex justify-content-center">
+          <div class="card card-border" style="width: 38rem;">
+            <img src="/public/image/Wa.jpg" class="card-img-top" alt="WhatsApp" style="object-fit:cover; height: 200px;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button class="btn btn-primary">Go somewhere</button>
             </div>
+          </div>
         </div>
       </div>
     </div>
-
-    <!-- SVG Definitions -->
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-      <symbol id="home" viewBox="0 0 16 16">
-        <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
-      </symbol>
-      <symbol id="speedometer2" viewBox="0 0 16 16">
-        <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
-        <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
-      </symbol>
-      <symbol id="table" viewBox="0 0 16 16">
-        <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z"/>
-      </symbol>
-    </svg>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
