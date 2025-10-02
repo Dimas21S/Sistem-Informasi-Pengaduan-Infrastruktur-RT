28db92c387e576e4790b9b41257524a440b9c3c6
Dimas Satyo Wijayanto <dewiandrianiii75@gmail.com>
2025-10-02 19:08:59 +0700

    Add: UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    // Fungsi untuk menampilkan profil pengguna
+    public function getProfile()
+    {
+        $user = Auth::user();
+
+        return view('profil', compact('user'));
+    }
+}
